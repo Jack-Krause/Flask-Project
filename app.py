541842616cb3bc1bf9417f1b1114a587bc1804e8
Jack Krause <93222059+Jack-Krause@users.noreply.github.com>
2021-12-13 12:32:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask import render_template
 from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+
 
 
 app = Flask(__name__)
@@ -10,6 +14,13 @@ def home():
     if len(request.form) > 0:
         username = request.form["name"]
         password = request.form["password"]
+
+
+app.route('plans', methods=["GET", "POST"])
+def plans():
+    return render_template("plans.html")
+
+
 
 if __name__ =='__main__':
     app.run(debug=True)
