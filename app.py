0157cbd5890ed74b5029136b2b7737505aa52528
Jack Krause <93222059+Jack-Krause@users.noreply.github.com>
2021-12-21 22:41:42 -0600

    Update app.py
    Working on the Flask form on the home page

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField
+from wtforms.fields.simple import SubmitField
 from wtforms.validators import DataRequired
 
-
-
-
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret_string"
+
+class MyForm(FlaskForm):
+    user_field = StringField("Enter username:", validators=[DataRequired()])
+    pass_field = StringField("Enter password:", validators=[DataRequired()])
+    subb = SubmitField()
+
 @app.route('/', methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
-    if len(request.form) > 0:
-        username = request.form["name"]
-        password = request.form["password"]
+    my_form = MyForm()
+    if my_form.validate_on_submit():
+        password = MyForm.pass_field.StringField
+        username = MyForm.user_field.StringField
+        return redirect(url_for("plans.html"))
 
-
-app.route('plans', methods=["GET", "POST"])
-def plans():
-    return render_template("plans.html")
-
-
+# app.route('plans', methods=["GET", "POST"])
+# def plans():
+#     return render_template("plans.html")
 
 if __name__ =='__main__':
     app.run(debug=True)
